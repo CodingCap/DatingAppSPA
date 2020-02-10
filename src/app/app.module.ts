84e707ba0ebc88cfaac69ea394dcfda862e46610
Albu Valentin <albuvalentin23@yahoo.com>
2020-02-10 23:42:59 +0200

    end of section 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import {AuthService} from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import {ErrorInterceptorProvider} from './_services/error.interceptor';
+import {AlertifyService} from './_services/alertify.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,11 +25,14 @@ import {ErrorInterceptorProvider} from './_services/error.interceptor';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
     AuthService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AlertifyService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AuthService} from '../_services/auth.service';
+import {AlertifyService} from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,19 +12,19 @@ export class RegisterComponent implements OnInit {
   // output properties emits events
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private authService: AuthService) { }
+
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
   register() {
     this.authService.register(this.model)
-      .subscribe(() => console.log('regiostration succesull'),
+      .subscribe(() => this.alertify.success('regiostration succesull'),
         error => {
-          console.log(error);
+          this.alertify.error(error);
         });
   }
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('canceled');
   }
 }
